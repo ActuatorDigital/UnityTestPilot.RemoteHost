fe82ef167564ee04acf37767df460ce76f31f8b5
Sebastiaan Fehr <seb@animaginedreality.com>
2021-02-01 17:03:14 +1000

    Standardized RemoteElementQuery.

--- Runtime/IRemoteUnityDriver.cs
+++ Runtime/IRemoteUnityDriver.cs
@@ -1,107 +1,65 @@
-﻿// Copyright (c) AIR Pty Ltd. All rights reserved.
-
-using System;
+﻿using System;
 using System.Text;
 using System.Threading.Tasks;
 using TachyonCommon;
 
-namespace AIR.UnityTestPilotRemote.Common
-{
-
-    public enum QueryFormat
-    {
-        /// <summary>
-        /// Query which converts to By.Name call in TestPilot Native.
-        /// </summary>
-        NamedQuery = 0,
-
-        /// <summary>
-        /// Query which converts to By.Type call in TestPilot Native.
-        /// </summary>
-        TypedQuery = 1,
-    }
+namespace AIR.UnityTestPilotRemote.Common {
 
     [GenerateBindings]
-    public interface IRemoteUnityDriver
-    {
+    public interface IRemoteUnityDriver {    
         Task<RemoteUiElement> Query(RemoteElementQuery query);
-        void Shutdown(bool immediate);
+        void Shutdown(Boolean immediate);
         void SetTimeScale(float timeScale);
         void LeftClick(RemoteUiElement element);
         void LeftClickDown(RemoteUiElement element);
         void LeftClickUp(RemoteUiElement element);
     }
 
-    public interface ISerializableAgentMessage
-    {
-        byte[] Serialize();
-        void Deserialize(byte[] objBytes);
-    }
+    public struct RemoteUiElement : ISerializableAgentMessage {
 
-    public struct RemoteUiElement : ISerializableAgentMessage
-    {
         public string Name;
         public bool IsActive;
         public string Text;
         public float XPos, YPos, ZPos;
         public float XRot, YRot, ZRot;
 
-        public RemoteUiElement(
-            string name,
-            bool isActive,
-            string text,
-            float xPos, float yPos, float zPos,
-            float xRot, float yRot, float zRot
-        ) {
-            Name = name;
-            IsActive = isActive;
-            Text = text;
-
-            XPos = xPos;
-            YPos = yPos;
-            ZPos = zPos;
-
-            XRot = xRot;
-            YRot = yRot;
-            ZRot = zRot;
-        }
-
-        public byte[] Serialize()
-        {
+        public byte[] Serialize() {
             var elementStr = String.Join("|",
-                Name,
+                Name, 
                 IsActive ? "Active" : "Inactive",
                 XPos, YPos, ZPos,
                 XRot, YRot, ZRot,
-                Text);
+                Text );
             return Encoding.ASCII.GetBytes(elementStr);
         }
 
         public void Deserialize(byte[] objBytes)
         {
             var elementStr = Encoding.ASCII.GetString(objBytes);
+            if(string.IsNullOrEmpty(elementStr))
+                return;
+
             var elementParts = elementStr.Split('|');
             Name = elementParts[0];
             IsActive = elementParts[1] == "Active";
+
             XPos = float.Parse(elementParts[2]);
             YPos = float.Parse(elementParts[3]);
             ZPos = float.Parse(elementParts[4]);
-            XPos = float.Parse(elementParts[5]);
-            YPos = float.Parse(elementParts[6]);
-            ZPos = float.Parse(elementParts[7]);
+
+            XRot = float.Parse(elementParts[5]);
+            YRot = float.Parse(elementParts[6]);
+            ZRot = float.Parse(elementParts[7]);
+
             Text = elementParts[8];
         }
     }
 
-    public struct RemoteElementQuery : ISerializableAgentMessage
-    {
-        public QueryFormat Format;
-        public string Name;
-        public string TargetType;
-
+    public struct RemoteElementQuery : ISerializableAgentMessage {
+    
         public RemoteElementQuery(
-            QueryFormat format,
-            string name,
+            QueryFormat format, 
+            string name, 
             string targetType
         ) {
             Format = format;
@@ -109,20 +67,21 @@ namespace AIR.UnityTestPilotRemote.Common
             TargetType = targetType;
         }
 
-        public byte[] Serialize()
-        {
-            var queryString = string.Join(
-                "|",
-                Format.ToString(),
-                Name,
+        public QueryFormat Format;
+        public string Name;
+        public string TargetType;
+
+        public byte[] Serialize() {
+            var queryString = String.Join("|", 
+                Format.ToString(), 
+                Name, 
                 TargetType
             );
 
             return Encoding.ASCII.GetBytes(queryString);
         }
 
-        public void Deserialize(byte[] objBytes)
-        {
+        public void Deserialize(byte[] objBytes) {
             var queryString = Encoding.ASCII.GetString(objBytes);
             var elementParts = queryString.Split('|');
             Format = (QueryFormat)Enum.Parse(typeof(QueryFormat), elementParts[0]);
@@ -130,4 +89,11 @@ namespace AIR.UnityTestPilotRemote.Common
             TargetType = elementParts[2];
         }
     }
+
+    public enum QueryFormat {
+        NamedQuery = 0,
+        TypedQuery = 1,
+        Invalid
+    }
+    
 }
